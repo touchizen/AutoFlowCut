a51d8d643134553dad9c0a556fb4b5b27bfd43e8
Gordon Ahn <gordon.ahn@gmail.com>
2026-05-23 14:05:55 +0900

    chore(auth): remove unused beforeEach import from withAuthRetry test

--- tests/utils/withAuthRetry.test.js
+++ tests/utils/withAuthRetry.test.js
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi, beforeEach } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 import { createAuthRetryWrapper } from '../../src/utils/withAuthRetry'
 
 function setup(overrides = {}) {
